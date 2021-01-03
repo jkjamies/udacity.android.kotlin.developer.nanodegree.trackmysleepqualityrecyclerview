b5199a1c2b60b03f4a3a6e8dd08352a63dbfdddc
jkjamies <jkjamies@mtu.edu>
2021-01-03 12:09:23 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # udacity.android.kotlin.developer.nanodegree.trackmysleepqualityrecyclerview
 TrackMySleepQualityRecyclerView Application from Udacity Android Kotlin Developer Nanodegree program.
+
+Highlights:
+
+RecyclerViews\
+\
+RecyclerView Adapters\
+\
+getItemCount, onBindViewHolder, onCreateViewHolder, notifyDataSetChanged()\
+\
+Custom XML Layout for RecyclerView and ViewHolder\
+\
+Highlight code segment, right click, refactor, function (in adapter, then alt+enter on holder, convert parameter to receiver, copy function into ViewHolder class, remove res variable from onBindViewHolder and parameter and add res variable in copied function in ViewHolder class).
+You can also use this for the onCreateViewHolder content by placing it in a function in a companion object inside the ViewHolder class with a parent ViewGroup parameter. Then changing the ViewHolder class to private constructor, as there is no need to call it from outside of the class anymore.\
+\
+Improving Data Refresh with DiffUtil (or notify item moved/updated/removed/changed/added)\
+\
+DataBinding to Adapter (alt+enter on root layout in xml and convert to binding layout as a tip, update companion object function inflate layout, update ViewHolder class parameter/type and RecyclerView.ViewHolder(binding.root)).
+Adding data to the xml views for DataBinding requires adding a BindingUtils file with annotated @BindingAdapter functions to convert to proper formatted resources, then in xml to set to [app:functionNameCreated="@{xmlVariable}"]\
+\
+GridLayout\
+\
+OnClickListeners for RecyclerView Items in ViewHolder -> ViewModel\
+\
+Navigate during on Click\
+\
+RecyclerView Headers (change definition of SleepNightAdapter.ViewHolder to RecyclerView.ViewHolder, and ListAdapter to hold type DataItem instead of SleepNight)\
+\
+Customize spans in RecyclerView (GridLayout)
---
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -0,0 +1,170 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.trackmysleepquality.sleeptracker
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.database.SleepNight
+import com.example.android.trackmysleepquality.databinding.ListItemSleepNightBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+//https://developer.android.com/reference/android/support/v7/widget/RecyclerView
+//https://developer.android.com/reference/android/support/v7/widget/RecyclerView.Adapter
+//https://developer.android.com/guide/topics/ui/layout/recyclerview
+
+private val ITEM_VIEW_TYPE_HEADER = 0
+private val ITEM_VIEW_TYPE_ITEM = 1
+
+class SleepNightAdapter(val clickListener: SleepNightListener) : ListAdapter<DataItem,
+        RecyclerView.ViewHolder>(SleepNightDiffCallback()) {
+
+    /**
+     * Use background thread then back to main thread
+     */
+    private val adapterScope = CoroutineScope(Dispatchers.Default)
+
+    /**
+     * When list is null just a header, else add header then add the list of sleep nights after
+     * Doing this on background thread back to main thread using adapterScope variable
+     */
+    fun addHeaderAndSubmitList(list: List<SleepNight>?) {
+        adapterScope.launch {
+            val items = when (list) {
+                null -> listOf(DataItem.Header)
+                else -> listOf(DataItem.Header) + list.map { DataItem.SleepNightItem(it) }
+            }
+            withContext(Dispatchers.Main) {
+                submitList(items)
+            }
+        }
+    }
+
+    /**
+     * bind view holders, only the nights get click listeners - not headers
+     */
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is ViewHolder -> {
+                val nightItem = getItem(position) as DataItem.SleepNightItem
+                holder.bind(clickListener, nightItem.sleepNight)
+            }
+        }
+    }
+
+    /**
+     * defining the viewholder types for creation
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            ITEM_VIEW_TYPE_HEADER -> TextViewHolder.from(parent)
+            ITEM_VIEW_TYPE_ITEM -> ViewHolder.from(parent)
+            else -> throw ClassCastException("Unknown viewType $viewType")
+        }
+    }
+
+    /**
+     * inflating the header text view holder
+     */
+    class TextViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        companion object {
+            fun from(parent: ViewGroup): TextViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater.inflate(R.layout.header, parent, false)
+                return TextViewHolder(view)
+            }
+        }
+    }
+
+    /**
+     * get the item view type (header vs sleep night)
+     */
+    override fun getItemViewType(position: Int): Int {
+        return when (getItem(position)) {
+            is DataItem.Header -> ITEM_VIEW_TYPE_HEADER
+            is DataItem.SleepNightItem -> ITEM_VIEW_TYPE_ITEM
+        }
+    }
+
+    /**
+     * SleepNight view holder class
+     */
+    class ViewHolder private constructor(val binding: ListItemSleepNightBinding)
+        : RecyclerView.ViewHolder(binding.root) {
+
+        /**
+         * SleepNight binding
+         */
+        fun bind(clickListener: SleepNightListener, item: SleepNight) {
+            binding.sleep = item
+            binding.clickListener = clickListener
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ListItemSleepNightBinding.inflate(layoutInflater, parent, false)
+
+                return ViewHolder(binding)
+            }
+        }
+    }
+}
+
+/**
+ * Callback for calculating the diff between two non-null items in a list.
+ *
+ * Used by ListAdapter to calculate the minumum number of changes between and old list and a new
+ * list that's been passed to `submitList`.
+ */
+class SleepNightDiffCallback : DiffUtil.ItemCallback<DataItem>() {
+    override fun areItemsTheSame(oldItem: DataItem, newItem: DataItem): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: DataItem, newItem: DataItem): Boolean {
+        return oldItem == newItem
+    }
+}
+
+/**
+ * click listener for recyclerview items
+ */
+class SleepNightListener(val clickListener: (sleepId: Long) -> Unit) {
+    fun onClick(night: SleepNight) = clickListener(night.nightId)
+}
+
+// Sealed class defines a closed type - all subclasses of DataItem must be defined in this file
+sealed class DataItem {
+    data class SleepNightItem(val sleepNight: SleepNight) : DataItem() {
+        override val id = sleepNight.nightId
+    }
+
+    object Header : DataItem() {
+        override val id = Long.MIN_VALUE
+    }
+
+    abstract val id: Long
+}
